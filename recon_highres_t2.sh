d0e13608c1ba0951f123a74e4266564de1d12890
Jiook Cha <cha.jiook@gmail.com>
2017-06-23 23:07:02 -0400

    Update recon_highres_t2.sh

--- recon_highres_t2.sh
+++ recon_highres_t2.sh
@@ -9,7 +9,7 @@ IMPATH=/ifs/scratch/pimri/posnerlab/1anal/IDP/${year}/${s}
 EXPERTOPT=$SUBJECTS_DIR/expert.opt
 FLAIR=`ls $IMPATH/3DFLAIR*nii`
 T1=`ls $IMPATH/3DT1*nii`
-SUBJECT=${s}.05mm.flair
+SUBJECT=${s}_05mm_flair
 CMD=$SUBJECTS_DIR/logs/cmd.${s}
 recon=$SUBJECTS_DIR/logs/recon.${s}
 echo "recon-all -all -s $SUBJECT -hires -i $T1 -expert $EXPERTOPT -FLAIR $FLAIR -FLAIRpial -hippocampal-subfields-T1 -openmp 32 -hemi lh" > $recon
