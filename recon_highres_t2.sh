762000db721c8c8f6d276f27e8e9c97b881b9ad1
Jiook Cha <cha.jiook@gmail.com>
2017-06-24 21:24:38 -0400

    Update recon_highres_t2.sh

--- recon_highres_t2.sh
+++ recon_highres_t2.sh
@@ -12,7 +12,15 @@ T1=`ls $IMPATH/t1*nii`
 SUBJECT=${s}_05mm_flair
 CMD=/ifs/scratch/pimri/posnerlab/1anal/IDP/code/idp/job/cmd.${s}
 recon=/ifs/scratch/pimri/posnerlab/1anal/IDP/code/idp/job/recon.${s}
-echo "recon-all -all -s $SUBJECT -hires -i $T1 -expert $EXPERTOPT -FLAIR $FLAIR -FLAIRpial -hippocampal-subfields-T1 -openmp 32 -hemi lh" > $recon
+
+cat<<EOC >$recon
+#!/bin/bash
+FREESURFER_HOME=/ifs/home/msph/epi/jep2111/app/freesurfer/
+source $FREESURFER_HOME/SetUpFreeSurfer.sh
+echo NOW PERFORMING RECON-ALL
+recon-all -all -s $SUBJECT -hires -i $T1 -expert $EXPERTOPT -FLAIR $FLAIR -FLAIRpial -hippocampal-subfields-T1 -openmp 32 -hemi lh
+EOC
+
 chmod +x $recon
 
 
@@ -20,7 +28,7 @@ cat<<-EOM >$CMD
 #!/bin/bash
 #$ -V
 #$ -cwd -S /bin/bash -N mpiprog
-#$ -l mem=3G,time=24::
+#$ -l mem=1G,time=24::
 #$ -pe orte 32
 #$ -l infiniband=TRUE
 source /ifs/home/msph/epi/jep2111/.bashrc
