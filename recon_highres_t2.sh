cd672566b7b9d3a9186f11e9866b28d3bea82a5d
Jiook Cha <cha.jiook@gmail.com>
2017-07-14 15:48:20 -0400

    Update recon_highres_t2.sh

--- recon_highres_t2.sh
+++ recon_highres_t2.sh
@@ -9,7 +9,7 @@ IMPATH=/ifs/scratch/pimri/posnerlab/1anal/IDP/${year}/${s}
 EXPERTOPT=$SUBJECTS_DIR/expert.opt
 FLAIR=`ls $IMPATH/flair*nii`
 T1=`ls $IMPATH/t1*nii`
-SUBJECT=${s}_05mm_flair
+SUBJECT=${s}_1mm_flair
 CMD=/ifs/scratch/pimri/posnerlab/1anal/IDP/code/idp/job/cmd.${s}
 recon=/ifs/scratch/pimri/posnerlab/1anal/IDP/code/idp/job/recon.${s}
 
@@ -20,7 +20,7 @@ source $FREESURFER_HOME/SetUpFreeSurfer.sh
 SUBJECTS_DIR=/ifs/scratch/pimri/posnerlab/1anal/IDP/fs
 echo NOW PERFORMING RECON-ALL
 #recon-all -all -s ${SUBJECT}.test_mpi128 -hires -i $T1 -expert $EXPERTOPT -FLAIR $FLAIR -FLAIRpial -hippocampal-subfields-T1 -openmp 64 
-recon-all -all -s ${SUBJECT}_test_mpi24 -hires -i $T1 -expert $EXPERTOPT -FLAIR $FLAIR -FLAIRpial -hippocampal-subfields-T1T2 $FLAIR flair -openmp 24
+recon-all -all -s ${SUBJECT}_test_mpi12 -i $T1 -expert $EXPERTOPT -FLAIR $FLAIR -FLAIRpial -hippocampal-subfields-T1T2 $FLAIR flair -openmp 12
 EOC
 
 chmod +x $recon
@@ -29,9 +29,9 @@ chmod +x $recon
 cat<<-EOM >$CMD
 #!/bin/bash
 #$ -V
-#$ -cwd -S /bin/bash -N reconhigh
-#$ -l mem=3G,time=168::
-#$ -pe orte 24
+#$ -cwd -S /bin/bash -N recon
+#$ -l mem=6G,time=72::
+#$ -pe orte 12
 #$ -l infiniband=TRUE
 source /ifs/home/msph/epi/jep2111/.bashrc
 . /nfs/apps/openmpi/current/setenv.sh
