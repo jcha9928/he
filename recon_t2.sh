5f925815e9941ff699aa6c4cb1733cce34257cf2
Jiook Cha <cha.jiook@gmail.com>
2017-07-19 13:31:49 -0400

    Update recon_t2.sh

--- recon_t2.sh
+++ recon_t2.sh
@@ -45,7 +45,7 @@ source /ifs/home/msph/epi/jep2111/.bashrc
 mpirun $recon1
 EOM
 
-prejobid=`qsub $CMD1`
+#prejobid=`qsub $CMD1`
 echo $CMD1
 
 ### 2 HIPPOCAMPAL SEGMENTATION
@@ -73,4 +73,5 @@ source /ifs/home/msph/epi/jep2111/.bashrc
 $recon2
 EOM
 
-qsub -hold_jid $prejobid $CMD2
+#qsub -hold_jid $prejobid $CMD2
+echo $CMD2
