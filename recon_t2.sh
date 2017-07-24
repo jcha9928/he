e9a3a0df2c9b2174583e51a4303133b81c2141a4
Jiook Cha <cha.jiook@gmail.com>
2017-07-24 15:21:28 -0400

    Update recon_t2.sh

--- recon_t2.sh
+++ recon_t2.sh
@@ -25,12 +25,6 @@ rm -rf $recon1
 
 ### 1 INITIAL RECON-ALL
 cat<<EOC >$recon1
-#!/bin/bash
-FREESURFER_HOME=/ifs/home/msph/epi/jep2111/app/freesurfer/
-source $FREESURFER_HOME/SetUpFreeSurfer.sh
-SUBJECTS_DIR=/ifs/scratch/pimri/posnerlab/1anal/IDP/fs
-echo NOW PERFORMING RECON-ALL
-#recon-all -all -s ${SUBJECT}.test_mpi128 -hires -i $T1 -expert $EXPERTOPT -FLAIR $FLAIR -FLAIRpial -hippocampal-subfields-T1 -openmp 64 
 recon-all -all -s ${SUBJECT} -i $T1 -FLAIR $FLAIR -FLAIRpial -qcache
 EOC
 
@@ -43,7 +37,7 @@ echo $recon1
 EOM
 done
 
-#prejobid=`qsub $CMD1 | awk '{print $3}'`
+#prejobid=`$code/fsl_sub_hpc_6 -t $CMD1`
 echo $CMD1
 
 for s in `cat $hr/fs/$list`
@@ -53,12 +47,6 @@ rm -rf $recon2
 
 ### 2 HIPPOCAMPAL SEGMENTATION
 cat<<EOC >$recon2
-#!/bin/bash
-FREESURFER_HOME=/ifs/home/msph/epi/jep2111/app/freesurfer/
-source $FREESURFER_HOME/SetUpFreeSurfer.sh
-SUBJECTS_DIR=/ifs/scratch/pimri/posnerlab/1anal/IDP/fs
-echo NOW PERFORMING RECON-ALL
-#recon-all -all -s ${SUBJECT}.test_mpi128 -hires -i $T1 -expert $EXPERTOPT -FLAIR $FLAIR -FLAIRpial -hippocampal-subfields-T1 -openmp 64 
 recon-all -s ${SUBJECT} -hippocampal-subfields-T1T2 $FLAIR flair -itkthreads ${threads} 
 EOC
 
@@ -70,5 +58,5 @@ echo $recon2
 EOM
 done
 
-#qsub -hold_jid $prejobid $CMD2
+#$code/fsl_sub_hpc_3 -hold_jid $prejobid -t $CMD2
 echo $CMD2
