a877b994061a8363b2c3b7a98175a8f070fb353c
Jiook Cha <cha.jiook@gmail.com>
2017-07-24 14:16:00 -0400

    Update recon_t2.sh

--- recon_t2.sh
+++ recon_t2.sh
@@ -2,9 +2,15 @@
 
 list=$1
 #year=2011
+threads=6
 
-s=10004362
-threads=8
+CMD1=/ifs/scratch/pimri/posnerlab/1anal/IDP/code/idp/job/cmd1.${list}
+CMD2=/ifs/scratch/pimri/posnerlab/1anal/IDP/code/idp/job/cmd2.${list}
+rm -rf $CMD1
+rm -rf $CMD2
+
+for s in `cat $hr/fs/$list`
+do
 
 SUBJECTS_DIR=/ifs/scratch/pimri/posnerlab/1anal/IDP/fs
 
@@ -13,11 +19,9 @@ EXPERTOPT=$SUBJECTS_DIR/expert.opt
 FLAIR=`ls $IMPATH/flair*nii*`
 T1=`ls $IMPATH/t1*nii*`
 SUBJECT=${s}_1mm_flair_test
-CMD1=/ifs/scratch/pimri/posnerlab/1anal/IDP/code/idp/job/cmd1.${s}
-CMD2=/ifs/scratch/pimri/posnerlab/1anal/IDP/code/idp/job/cmd2.${s}
 
 recon1=/ifs/scratch/pimri/posnerlab/1anal/IDP/code/idp/job/recon1.${s}
-recon2=/ifs/scratch/pimri/posnerlab/1anal/IDP/code/idp/job/recon2.${s}
+rm -rf $recon1
 
 ### 1 INITIAL RECON-ALL
 cat<<EOC >$recon1
@@ -33,18 +37,19 @@ EOC
 chmod +x $recon1
 
 
-cat<<-EOM >$CMD1
-#!/bin/bash
-#$ -V
-#$ -cwd -S /bin/bash -N recon1
-#$ -l mem=9G,time=72::
-#$ -o /ifs/scratch/pimri/posnerlab/1anal/IDP/code/idp/job -e /ifs/scratch/pimri/posnerlab/1anal/IDP/code/idp/job
-source /ifs/home/msph/epi/jep2111/.bashrc
-$recon1
-EOM
 
-prejobid=`qsub $CMD1 | awk '{print $3}'`
-#echo $CMD1
+cat<<-EOM >$CMD1
+echo $recon1
+EOM
+done
+
+#prejobid=`qsub $CMD1 | awk '{print $3}'`
+echo $CMD1
+
+for s in `cat $hr/fs/$list`
+do
+recon2=/ifs/scratch/pimri/posnerlab/1anal/IDP/code/idp/job/recon2.${s}
+rm -rf $recon2
 
 ### 2 HIPPOCAMPAL SEGMENTATION
 cat<<EOC >$recon2
@@ -61,15 +66,9 @@ chmod +x $recon2
 
 
 cat<<-EOM >$CMD2
-#!/bin/bash
-#$ -V
-#$ -cwd -S /bin/bash -N recon2
-#$ -l mem=3G,time=24::
-#$ -pe smp ${threads}
-#$ -o /ifs/scratch/pimri/posnerlab/1anal/IDP/code/idp/job -e /ifs/scratch/pimri/posnerlab/1anal/IDP/code/idp/job
-source /ifs/home/msph/epi/jep2111/.bashrc
-$recon2
+echo $recon2
 EOM
+done
 
-qsub -hold_jid $prejobid $CMD2
-#echo $CMD2
+#qsub -hold_jid $prejobid $CMD2
+echo $CMD2
